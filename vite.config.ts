515bbbbd70db09f3f9bac7f0c0f2b471f6b4b928
3starblaze <3starblaze@protonmail.com>
2026-01-30 12:54:29 +0200

    Externalize peer dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import dts from 'vite-plugin-dts'
+import { peerDependencies } from "./package.json"
 
 const libMode = true;
 
@@ -29,6 +30,9 @@ export default defineConfig({
         entry: path.resolve(__dirname, "./src/lib_index.ts"),
         name: "rdf-toolbag",
         fileName: "rdf-toolbag",
+      },
+      rollupOptions: {
+        external: [...Object.keys(peerDependencies)],
       },
     },
   } : {}),
