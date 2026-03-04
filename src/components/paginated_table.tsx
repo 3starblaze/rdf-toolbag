315c035f158b3ac9f48025dcf450efde156de790
3starblaze <3starblaze@protonmail.com>
2026-03-04 21:35:59 +0200

    Make resizable columns in table
    Ignoring the new drag handles, this change introduces changes in how
    tables are shown. In the current UI this change isn't always desirable
    and needs to be fixed in the future.

--- src/components/paginated_table.tsx
+++ src/components/paginated_table.tsx
@@ -12,6 +12,7 @@ import {
     getPaginationRowModel,
     useReactTable,
     type ColumnDef,
+    type Header,
     type OnChangeFn,
     type PaginationState,
 } from "@tanstack/react-table";
@@ -29,6 +30,7 @@ import {
     SelectTrigger,
     SelectValue,
 } from "./ui/select";
+import { GripVerticalIcon } from "lucide-react";
 
 export const defaultPagination: PaginationState = {
     pageIndex: 0,
@@ -52,6 +54,35 @@ function TableButton({
         </Button>
     );
 }
+
+export function ColumnResizer<TData, TValue>({
+    header,
+}: {
+    header: Header<TData, TValue>,
+}) {
+    if (header.column.getCanResize() === false) return <></>;
+
+    // NOTE: Style is similar to shadcn's "Collapsible" component's handle
+    return (
+        <div
+            className="relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:outline-hidden aria-[orientation=horizontal]:h-px aria-[orientation=horizontal]:w-full aria-[orientation=horizontal]:after:left-0 aria-[orientation=horizontal]:after:h-1 aria-[orientation=horizontal]:after:w-full aria-[orientation=horizontal]:after:translate-x-0 aria-[orientation=horizontal]:after:-translate-y-1/2 [&[aria-orientation=horizontal]>div]:rotate-90"
+            // NOTE: these styles ensure that text selection (and other) behavior won't kick in
+            // while dragging is in progress.
+            style={{
+                userSelect: "none",
+                touchAction: "none",
+            }}
+        >
+            <div
+                onMouseDown={header.getResizeHandler()}
+                onTouchStart={header.getResizeHandler()}
+                className="cursor-col-resize z-10 flex h-4 w-3 items-center justify-center rounded-xs border bg-border"
+            >
+                <GripVerticalIcon className="size-2.5" />
+            </div>
+        </div >
+    );
+};
 
 export default function PaginatedTable<TData>({
     data,
@@ -86,11 +117,17 @@ export default function PaginatedTable<TData>({
         state: {
             pagination,
         },
+        enableColumnResizing: true,
+        columnResizeMode: "onChange",
     });
 
     return (
         <div>
-            <Table>
+            <Table
+                style={{
+                    width: table.getCenterTotalSize()
+                }}
+            >
                 <TableHeader>
                     {table.getHeaderGroups().map((headerGroup) => (
                         <TableRow key={headerGroup.id}>
@@ -99,13 +136,21 @@ export default function PaginatedTable<TData>({
                                     <TableHead
                                         className="font-bold"
                                         key={header.id}
+                                        style={{
+                                            width: header.getSize(),
+                                        }}
                                     >
-                                        {header.isPlaceholder
-                                            ? null
-                                            : flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext()
-                                            )}
+                                        <div className="flex flex-row justify-between gap-4 h-full">
+                                            <div>
+                                                {header.isPlaceholder
+                                                    ? null
+                                                    : flexRender(
+                                                        header.column.columnDef.header,
+                                                        header.getContext()
+                                                    )}
+                                            </div>
+                                            <ColumnResizer header={header} />
+                                        </div>
                                     </TableHead>
                                 )
                             })}
@@ -120,7 +165,12 @@ export default function PaginatedTable<TData>({
                                 data-state={row.getIsSelected() && "selected"}
                             >
                                 {row.getVisibleCells().map((cell) => (
-                                    <TableCell key={cell.id}>
+                                    <TableCell
+                                        key={cell.id}
+                                        style={{
+                                            width: cell.column.getSize(),
+                                        }}
+                                    >
                                         {flexRender(cell.column.columnDef.cell, cell.getContext())}
                                     </TableCell>
                                 ))}
