0cf72319a5cf45886e1bf26968889269d84d6c45
3starblaze <3starblaze@protonmail.com>
2026-04-12 14:44:15 +0300

    Implement demangleVarName

--- src/misc/complex_property_query_builder.test.ts
+++ src/misc/complex_property_query_builder.test.ts
@@ -1,34 +1,35 @@
 import { expect, test, describe } from "vitest";
-import { formatQuery } from "./complex_property_query_builder";
+import { demangleVarName, formatQuery } from "./complex_property_query_builder";
 import type { ComplexPropertySelection } from "@/components/complex_property_selector";
+
+const sampleSelection = {
+    rdfType: "http://Person",
+    dataProps: [{ name: "http://p0" }, { name: "http://p1" }],
+    objectProps: [
+        {
+            name: "http://relation",
+            selection: {
+                rdfType: "http://Item",
+                dataProps: [{ name: "http://p0" }],
+                objectProps: [
+                    {
+                        name: "http://anotherRelation",
+                        selection: {
+                            rdfType: "http://anotherRelatedType",
+                            dataProps: [
+                                { name: "http://lastDataPropISwear" }
+                            ],
+                            objectProps: [],
+                        },
+                    }
+                ],
+            },
+        },
+    ],
+} satisfies ComplexPropertySelection;
 
 describe("formatQuery", () => {
     test("basic test", () => {
-        const selection: ComplexPropertySelection = {
-            rdfType: "http://Person",
-            dataProps: [{ name: "http://p0" }, { name: "http://p1" }],
-            objectProps: [
-                {
-                    name: "http://relation",
-                    selection: {
-                        rdfType: "http://Item",
-                        dataProps: [{ name: "http://p0" }],
-                        objectProps: [
-                            {
-                                name: "http://anotherRelation",
-                                selection: {
-                                    rdfType: "http://anotherRelatedType",
-                                    dataProps: [
-                                        { name: "http://lastDataPropISwear" }
-                                    ],
-                                    objectProps: [],
-                                },
-                            }
-                        ],
-                    },
-                },
-            ],
-        };
         const expectedVars = [
             "?this",
             "?this_d_0",
@@ -39,13 +40,42 @@ describe("formatQuery", () => {
             "?this_o_0_o_0_d_0",
         ];
 
-        const formattedQuery = formatQuery(selection);
+        const formattedQuery = formatQuery(sampleSelection);
         const foundVars = new Set(formattedQuery.match(/\?\w+/g));
 
         // NOTE: we should see all the expected vars in the query
         expect(expectedVars.every((varName) => foundVars.has(varName))).toBe(true);
 
         // NOTE: asserting one constraint that should appear
-        expect(formattedQuery).toMatch(`?this_o_0 <${selection.dataProps[0].name}> ?this_o_0_d_0`);
+        expect(formattedQuery).toMatch(`?this_o_0 <${sampleSelection.dataProps[0]?.name}> ?this_o_0_d_0`);
+    });
+});
+
+describe("demangleVarName", () => {
+    test("just this", () => {
+        expect(demangleVarName("this", sampleSelection)).toMatch("this");
+    });
+    test("data prop", () => {
+        expect(demangleVarName("this_d_1", sampleSelection)).toMatch("this > http://p1");
+    });
+    test("obj prop", () => {
+        expect(demangleVarName("this_o_0", sampleSelection)).toMatch("this > http://relation");
+    });
+    test("nested data prop", () => {
+        expect(demangleVarName("this_o_0_d_0", sampleSelection))
+            .toMatch("this > http://relation > http://p0");
+    });
+    test("deeper nesting", () => {
+        expect(demangleVarName("this_o_0_o_0_d_0", sampleSelection))
+            .toMatch("this > http://relation > http://anotherRelation > http://lastDataPropISwear");
+    });
+    test("garbage is rejected", () => {
+        expect(demangleVarName("garbage", sampleSelection)).toBe(null);
+    });
+    test("garbage starting with 'this' is rejected", () => {
+       expect(demangleVarName("this_garbage", sampleSelection)).toBe(null);
+    });
+    test("out of range property is rejected", () => {
+       expect(demangleVarName("this_d_99", sampleSelection)).toBe(null);
     });
 });
--- src/misc/complex_property_query_builder.ts
+++ src/misc/complex_property_query_builder.ts
@@ -1,4 +1,5 @@
 import type { ComplexPropertySelection } from "@/components/complex_property_selector";
+import { Match } from "effect";
 
 // NOTE: Made this helper function so that we can elegantly collect object properties.
 function collectConstraints(
@@ -33,4 +34,53 @@ export function formatQuery(selection: ComplexPropertySelection) {
     const fmt = `SELECT * WHERE { ${constraints.join("\n")} }`;
 
     return fmt;
+}
+
+/**
+ * Turn generated variable name into human-readable label.
+ *
+ * Null is returned when varName does not match selection.
+ *
+ * @param varName variable without the leading "?" symbol
+ * @param selection selection that is used for reference
+ */
+export function demangleVarName(
+    varName: string,
+    selection: ComplexPropertySelection,
+): string | null {
+    /**
+     *
+     * @param parts fragments from variable name
+     * @param labelParts list of already collected label fragments that will be combined
+     */
+    function resolve(
+        parts: string[],
+        labelParts: string[],
+        currentSelection: ComplexPropertySelection,
+    ): string[] | null {
+        return Match.value(parts.slice(0, 2)).pipe(
+            Match.when(["d", Match.any], ([_, indexString]) => {
+                const maybeLabelPart = currentSelection.dataProps[Number(indexString)]?.name;
+                return maybeLabelPart ? [...labelParts, maybeLabelPart] : null;
+            }),
+            Match.when(["o", Match.any], ([_, indexString]) => {
+                const maybeProp = currentSelection.objectProps[Number(indexString)];
+                if (!maybeProp) return null;
+                return resolve(
+                    parts.slice(2),
+                    [...labelParts, maybeProp.name],
+                    maybeProp.selection,
+                );
+            }),
+            // NOTE: We are finished and we can return what we have collected
+            Match.when((val) => val.length === 0, () => labelParts),
+            Match.orElse(() => null),
+        );
+    }
+
+    const [thisPart, ...restParts] = varName.split("_");
+    if (thisPart !== "this") return null;
+
+    const labelParts = resolve(restParts, ["this"], selection);
+    return labelParts ? labelParts.join(" > ") : null;
 }
