d2b862796587b036079a3e5f4e4a4c364497c094
3starblaze <3starblaze@protonmail.com>
2026-06-15 16:02:27 +0300

    Init storybook for multicardinal table

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,64 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import dts from 'vite-plugin-dts'
-import { peerDependencies } from "./package.json"
+/// <reference types="vitest/config" />
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import dts from 'vite-plugin-dts';
+import { peerDependencies } from "./package.json";
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 const libMode = true;
 const devMode = true;
 
-// https://vite.dev/config/
 export default defineConfig({
   // NOTE: Using relative path so that assets can be found no matter the absolute path
   base: "./",
-  plugins: [
-      react(),
-      tailwindcss(),
-      libMode && dts({
-        tsconfigPath: './tsconfig.app.json',
-        rollupTypes: true,
-      }),
-  ],
+  plugins: [react(), tailwindcss(), libMode && dts({
+    tsconfigPath: './tsconfig.app.json',
+    rollupTypes: true
+  })],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      "@": path.resolve(__dirname, "./src")
+    }
   },
   ...(libMode ? {
     build: {
       lib: {
         entry: path.resolve(__dirname, "./src/lib_index.ts"),
         name: "rdf-toolbag",
-        fileName: "rdf-toolbag",
+        fileName: "rdf-toolbag"
       },
       rollupOptions: {
-        external: [...Object.keys(peerDependencies)],
+        external: [...Object.keys(peerDependencies)]
       },
       // NOTE: If dev mode enabled, don't minify, otherwise keep the default setting
-      minify: devMode ? false : "esbuild",
-    },
+      minify: devMode ? false : "esbuild"
+    }
   } : {}),
-})
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        }
+      }
+    }]
+  }
+});
